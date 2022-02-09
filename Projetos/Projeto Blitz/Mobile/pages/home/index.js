df1be4a672227b0ece815ecaf47181c101dcad1a
LeoImenes <leonardoimenes@hotmail.com>
2022-02-09 19:17:04 -0300

    att

--- Projetos/Projeto Blitz/Mobile/pages/home/index.js
+++ Projetos/Projeto Blitz/Mobile/pages/home/index.js
@@ -1,10 +1,104 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { View, Text, Dimensions, StyleSheet, ToastAndroid, Image } from 'react-native';
+import { Marker } from 'react-native-maps';
+
+import * as Location from 'expo-location';
+
+import MapView from 'react-native-maps';
 
 export default function Home() {
+    const [coord, setCoord] = useState({
+        latitude: 37.78825,
+        longitude: -122.4324,
+    })
+    const [marcadores, setMarcadores] = useState([])
+    
+
+    useEffect(async () => {
+        let { status } = await Location.requestForegroundPermissionsAsync();
+
+        if (status !== 'granted') {
+            ToastAndroid.show('Localização negada', ToastAndroid.SHORT);
+        }else {
+            let location = await Location.getCurrentPositionAsync({accuracy: Location.Accuracy.BestForNavigation});
+
+            console.log(location);
+            setCoord({
+                latitude:location.coords.latitude,
+                longitude:location.coords.longitude
+                
+            });
+        
+            let posi = {
+                coordenadas: location.coords.latitude + ',' + location.coords.longitude,
+                alertum:{
+                    tipo :"email",
+                    descricao : "Minha localização"
+                },
+               imagem: require(`../../assets/App/Icons/senha.png`)
+            }
+            let arr = [];
+            arr.push(posi)
+            setMarcadores(arr);
+
+
+        }
+    }, []);
+
+    
+
+    useEffect(() => {
+        fetch("http://10.87.207.20:3000/local")
+        .then(resp => {
+            return resp.json()
+        }).then(data => {
+            setMarcadores(data.usuario)
+        }).catch(err => {
+        })
+    }, [marcadores])
+
     return(
-        <View>
-            <Text>Tela Home</Text>
+        <View style={styles.container}>
+            <MapView 
+            style={styles.map} 
+            region={{
+                ...coord,
+                latitudeDelta: 0.002,
+                longitudeDelta: 0.002,
+            }}>
+                { 
+                    console.log(marcadores)
+                    // marcadores.map((marcador, index)=>{
+                    //     let loc = marcador.coordenadas.split(",");
+                    //     let teste = "senha"
+                    //     let imagem = require(`../../assets/App/Icons/${teste}.png`);
+                    //     return(
+                    //         <Marker key={index} coordinate={
+                    //         { latitude: Number(loc[0]), 
+                    //          longitude: Number(loc[1])}} 
+                    //          title={marcador.alertum.tipo} 
+                    //          description={""}>
+                    //                <Image source={imagem} style={{width: 28, height: 28}}/>
+                    //          </Marker>
+                    //     )
+                        
+                    // })
+                }
+           
+            </MapView>
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#fff',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+    map: {
+      width: Dimensions.get('window').width,
+      height: Dimensions.get('window').height,
+    },
+});
--- Projetos/Projeto Blitz/Mobile/pages/login/index.js
+++ Projetos/Projeto Blitz/Mobile/pages/login/index.js
@@ -8,60 +8,103 @@ export default function Login({ navigation }) {
 
     const [email, setEmail] = useState("");
     const [senha, setSenha] = useState("");
+    const [cadastrando, setCadastrando] = useState(false);
 
-    const [imagem,setImagem] = useState(require('../../assets/App/images/user.png'));
+    const [imagem, setImagem] = useState(require('../../assets/App/images/user.png'));
 
     const autenticar = () => {
         let usuario = {
             email: email,
             senha: md5(senha),
         }
-        fetch("http://192.168.0.29:3000/login", {
+        fetch("http://10.87.207.20:3000/login", {
 
             "method": "POST",
             "headers": {
                 "Content-Type": "application/json"
             },
             "body": JSON.stringify(usuario)
-            })
-        .then(resp => { return resp.json() })
-        .then(async data => {
-            if (data.length > 0) {
-             await AsyncStorage.setItem('userdata', JSON.stringify(data[0]));
-            navigation.navigate("Main");
+        })
+            .then(resp => { return resp.json() })
+            .then(async data => {
+                if (data.length > 0) {
+                    await AsyncStorage.setItem('userdata', JSON.stringify(data[0]));
+                    navigation.navigate("Main");
                 } else {
-                ToastAndroid.show("Email ou Senha Invalidos", ToastAndroid.SHORT)
-                    }
-                })
+                    ToastAndroid.show("Email ou Senha Invalidos", ToastAndroid.SHORT)
+                }
+            })
     }
 
-   const selecionarImagem = async ()=>{
+    const selecionarImagem = async () => {
         let result = await ImagePicker.launchImageLibraryAsync({
             mediaTypes: ImagePicker.MediaTypeOptions.All,
-            allowsEditing: false,
+            allowsEditing: true,
             aspect: [4, 3],
             quality: 1,
-          });
-            if (!result.cancelled) {
-               setImagem({
-                    uri:result.uri
-                })
-          }
-      
+            base64: true
+        });
+        if (!result.cancelled && result.base64.length < 59500) {
+            setImagem({
+                uri: result.uri
+            })
+        } else if (!result.cancelled) {
+            ToastAndroid.show("Selecione outra imagem", ToastAndroid.SHORT)
+        }
+
     }
+
+    const habilitarCadastro = () => {
+        setCadastrando(true)
+    }
+
+    const cadastrar = () => {
+        let usuario = {
+            email: email,
+            senha: md5(senha),
+            foto: (imagem.uri !== undefined) ? imagem.uri : ''
+
+        }
+        fetch("http://10.87.207.20:3000/usuario",{
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(usuario)
+        })
+        .then(response => {return response.json()})
+        .then(async data => {
+            if(data.id === undefined) {
+                ToastAndroid.show("Falha ao cadastrar", ToastAndroid.SHORT)
+                setCadastrando(false)
+            }else{
+                await AsyncStorage.setItem('userdata', JSON.stringify(data))
+                navigation.navigate('Main')
+            }
+        })
+    }
+
 
     return (
         <View>
-            <Image source={imagem} style={{width: 128, height: 128}}/>
+            <Image source={imagem} style={{ width: 128, height: 128 }} />
             <TextInput value={email} onChangeText={setEmail} style={{ backgroundColor: 'lightblue' }} />
             <TextInput value={senha} onChangeText={setSenha} style={{ backgroundColor: 'lightblue' }} />
-            <TouchableOpacity onPress={() => selecionarImagem()} >
-           
-            <Image source={require('../../assets/App/images/camera.png')} style={{width: 32, height: 32}}/>
-            </TouchableOpacity>
 
-            <TouchableOpacity onPress={() => autenticar()} >
-                <Text>Login</Text>
+            {
+
+                (cadastrando) ?
+                    <TouchableOpacity hidden={true} onPress={() => { selecionarImagem() }}>
+                        <Image source={require("../../assets/App/images/camera.png")} style={{ width: 32, height: 32 }} />
+                    </TouchableOpacity>
+                    :
+                    <TouchableOpacity onPress={() => { habilitarCadastro() }}>
+                        <Text>Cadastrar-se</Text>
+                    </TouchableOpacity>
+            }
+
+            <TouchableOpacity onPress={() => cadastrar()} >
+                <Text>{(cadastrando)? "Cadastrar":"Login"}</Text>
             </TouchableOpacity>
         </View>
     )
